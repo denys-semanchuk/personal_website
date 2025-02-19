48dba284e2aa606679fe36fa2d73c96511196112
Denys Semanchuk <semanchukdenys@icloud.com>
2025-02-19 20:36:50 +0100

    added favicon and meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#8e44ad" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
