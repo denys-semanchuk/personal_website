0751607b31e48a88ca8e039fd209a121bd8c9bf4
denys-semanchuk <semanchukdenys@icloud.com>
2025-04-06 11:01:42 +0200

    added next pizza project to portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,15 +11,24 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
+    title: "Next Pizza",
+    description: "a fast and convenient pizza ordering service",
+    image: "/projects/project1.png",
+    technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Stripe", "Zustand"],
+    liveUrl: "https://next-pizza-phi-murex.vercel.app",
+    githubUrl: "https://github.com/denys-semanchuk/pizza"
+  },
+  {
+    id: 2,
     title: "TaskMaster ToDo App",
     description: "A modern task management application built with Next.js, Redux, and TypeScript",
     image: "/projects/project1.png",
     technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Redux"],
     liveUrl: "https://to-do-application-next-node.vercel.app/dashboard",
-    githubUrl: "https://github.com/username/project"
+    githubUrl: "https://github.com/denys-semanchuk/ToDo-Application-Next-Node"
   },
   {
-    id: 2,
+    id: 3,
     title: "Currency Converter",
     description: "Fast and reliable currency converter, using real-time exchange rates.",
     image: "/projects/project2.png",
@@ -28,7 +37,7 @@ export const projects: Project[] = [
     githubUrl: "https://github.com/denys-semanchuk/Currency-Converter"
   },
   {
-    id: 3,
+    id: 4,
     title: "QuickQuill Notes App",
     description: "A modern, feature-rich note-taking application.",
     image: "/projects/project3.png",
